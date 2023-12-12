4212c1cd49888caccd08ffe0d807b66dfb691f5a
BananaMan <suvorov.andrey755@gmail.com>
2023-12-13 02:45:22 +0300

    13.12

--- src/app/components/main/main.component.ts
+++ src/app/components/main/main.component.ts
@@ -1,36 +1,68 @@
-import { Component } from "@angular/core";
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { FormsModule, NgModel } from '@angular/forms';
 
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
   styleUrl: './main.component.css',
-  standalone: true
+  imports: [FormsModule, CommonModule],
+  standalone: true,
 })
 export class MainComponent {
-   name: string = "Жак-ив-кусто";
-   about: string = "Первооткрыватель"
-   openPopup() {
-    const popup = document.querySelector('.popup_open-edit')
-    popup?.classList.add('popup_opened')
-   }
-   closePopup() {
-    const popup = document.querySelector('.popup')
-    popup?.classList.remove('popup_opened')
-   }
-   openPopupCardAdd() {
-    const popup = document.querySelector('.popup_card-add')
-    popup?.classList.add('popup_opened')
-   }
-   closePopupCardAdd() {
-    const popup = document.querySelector('.popup_card-add')
-    popup?.classList.remove('popup_opened')
-   }
-   openPopupAvatar() {
-    const popup = document.querySelector('.popup_avatar')
-    popup?.classList.add('popup_opened')
-   }
-   closePopupAvatar() {
-    const popup = document.querySelector('.popup_avatar')
-    popup?.classList.remove('popup_opened')
-   }
+  item = [
+    {
+      image: './../../assets/images/dombai.png',
+      title: 'Домбай',
+    }
+  ]
+  ttl: string = ''
+  img: string = ''
+  name: string = 'Жак-ив-кусто';
+  about: string = 'Первооткрыватель';
+  avatar: string = '';
+  url: string = '../../../assets/images/avatar.png';
+  openPopup() {
+    const popup = document.querySelector('.popup_open-edit');
+    popup?.classList.add('popup_opened');
+  }
+  closePopup() {
+    const popup = document.querySelector('.popup');
+    popup?.classList.remove('popup_opened');
+  }
+  openPopupCardAdd() {
+    const popup = document.querySelector('.popup_card-add');
+    popup?.classList.add('popup_opened');
+  }
+  closePopupCardAdd() {
+    const popup = document.querySelector('.popup_card-add');
+    popup?.classList.remove('popup_opened');
+  }
+  openPopupAvatar() {
+    const popup = document.querySelector('.popup_avatar');
+    popup?.classList.add('popup_opened');
+  }
+  closePopupAvatar() {
+    const popup = document.querySelector('.popup_avatar');
+    popup?.classList.remove('popup_opened');
+  }
+  userInfo(name: string, about: string) {
+    name = name;
+    about = about;
+    this.closePopup();
+  }
+
+  likeEvent() {
+    const like = document.querySelector('.element__group-like')
+    like?.classList.toggle('element__group-like_active')
+  }
+
+  updateAvatar() {
+    this.url = this.avatar;
+    this.closePopupAvatar();
+  }
+  addCard() {
+    this.item.push({image: this.img, title: this.ttl})
+    this.closePopupCardAdd()
+  }
 }
